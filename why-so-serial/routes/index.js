1f2037b2daaae70426074d6f0095c57887797c7b
Patricio Castro <luckydrawings@gmail.com>
2020-08-03 11:17:41 +0200

    edit killer done

--- why-so-serial/routes/index.js
+++ why-so-serial/routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+    return;
+  }
+  res.redirect('/login');
+
+});
+
+router.get('/add-killer', (req, res, next) => {
+  res.render('private/add-killer');
+}); */
+
 
 module.exports = router;
--- why-so-serial/routes/private.js
+++ why-so-serial/routes/private.js
@@ -4,7 +4,23 @@ var router = express.Router();
 const Killer = require('../models/Killer');
 const User = require('../models/User');
 
+/* router.use((req, res, next) => {
+    if (req.session.currentUser) {
+        next();
+        return;
+    }
 
+    res.redirect('/login');
+}); */
+router.get('/killers', (req, res, next) => {
+    Killer.find()
+        .then(allKillers => {
+            res.render('killers', { killers: allKillers });
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+});
 
 router.get('/add-killer', (req, res, next) => {
     res.render('private/add-killer');
@@ -12,10 +28,10 @@ router.get('/add-killer', (req, res, next) => {
 
 
 router.post('/add-killer', (req, res, next) => {
-    const { author, name, lastName, aka, gender, murderType } = req.body;
-
+    const { name, lastName, aka, gender, murderType, author } = req.body;
+    /* const userId = req.session.currentUser._id; */
     const newKiller = new Killer({ name, lastName, aka, gender, murderType });
-    if (author === '' || name === '' || lastName === '' || murderType === '') {
+    if (name === '' || lastName === '' || murderType === '') {
         res.render('private/add-killer', { errorMessage: 'The fields: author, name, last name and murder type are required' });
         return;
     }
@@ -27,7 +43,7 @@ router.post('/add-killer', (req, res, next) => {
             }
             newKiller.save()
                 .then((killer) => {
-                    res.redirect('/');
+                    res.redirect('/killers');
                 })
                 .catch((err) => {
                     res.render('private/add-killer', { errorMessage: 'Error creating the new Serial Killer' });
@@ -39,6 +55,42 @@ router.post('/add-killer', (req, res, next) => {
 
 
 
+});
+
+router.get('/edit-killer', (req, res, next) => {
+    Killer.findOne({ _id: req.query.killer_id })
+        .then((killer) => {
+            res.render('private/edit-killer', { killer });
+        })
+        .catch((err) => {
+            console.log(error);
+        });
+
+});
+
+router.post('/edit-killer', (req, res, next) => {
+
+    const { name, lastName, aka, gender, murderType, birthDate, zodiacSign, yearsActive, numberOfVictimsConfirmed, numberOfVictimsPossible, country, weapons, arrested, victimProfile, description, books } = req.body;
+
+    Killer.update({ _id: req.query.killer_id },
+        { $set: { name, lastName, aka, gender, murderType, birthDate, zodiacSign, yearsActive, numberOfVictimsConfirmed, numberOfVictimsPossible, country, weapons, arrested, victimProfile, description, books } }, { new: true }
+    )
+        .then((killer) => {
+            res.redirect('/killers');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+router.get('/killer/:killerId', (req, res, next) => {
+    Killer.findById(req.params.killerId)
+        .then(theKiller => {
+            res.render('killer-details', { killer: theKiller });
+        })
+        .catch(error => {
+            console.log('error:', error);
+        });
 });
 
 
